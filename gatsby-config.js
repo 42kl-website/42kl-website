1df032574b53ac3e425e5f5955ddf420ce26e204
IsaaacLim <isaaclimjj@yahoo.com>
2021-09-06 14:30:24 +0800

    added static image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,11 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "42KL Website",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 };
