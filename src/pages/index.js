e39aa407cf5a6d7d0374ce3e10b24d665c57a001
IsaaacLim <isaaclimjj@yahoo.com>
2021-09-06 14:55:43 +0800

    added homepage image, index.css for StaticImage styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
 import * as React from "react";
 import LayoutHomePage from "../components/layout-homepage";
+import { StaticImage } from "gatsby-plugin-image";
+import "../../index.css";
+import styled from "styled-components";
 
-// markup
+//styles
+const Text = styled.div`
+  position: absolute;
+  bottom: 8px;
+  left: 16px;
+`;
+
+//markup
 const IndexPage = () => {
-  return <LayoutHomePage>Home page</LayoutHomePage>;
+  return (
+    <LayoutHomePage>
+      <StaticImage
+        src="../images/sample.jpg"
+        alt="Home page background image"
+        className="background"
+      />
+      <Text>Home page3</Text>
+    </LayoutHomePage>
+  );
 };
 
 export default IndexPage;
