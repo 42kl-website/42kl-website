57a51d683557f61e157e8d945e1fda03c685eadf
Kevin Loh <khaikhoon.loh@hotmail.com>
2021-09-23 13:05:04 +0800

    feat: GSAP animations integration

--- src/pages/animated.js
+++ src/pages/animated.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import {
   PlasmicComponent,
   PlasmicRootProvider,
@@ -6,6 +6,8 @@ import {
 } from "@plasmicapp/loader-gatsby";
 import { graphql } from 'gatsby';
 import Meteor42 from '../components/Meteor42';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 // Statically fetch the data needed to render Plasmic pages or components.
 // You can pass in multiple page paths or component names.
@@ -19,13 +21,31 @@ export const query = graphql`
 // Render the page or component from Plasmic.
 const Animated = ({ data }) => {
   const { plasmicComponents, plasmicOptions } = data;
-
   const PLASMIC = initPlasmicLoader(plasmicOptions);
-
   PLASMIC.registerComponent(Meteor42, {
     name: 'Meteor42',
     props: {
       width: 'string',
+    }
+  })
+
+  gsap.registerPlugin(ScrollTrigger)
+  const ref = useRef(null)
+  useEffect(() => {
+    if (ref.current) {
+      const element = ref.current;
+      gsap.to(
+        element.querySelector('.forty-two'),
+        {
+          scale: 6,
+          scrollTrigger: {
+            trigger: element.querySelector('.frame-140'),
+            start: "top bottom",
+            end: "+=300",
+            scrub: true,
+          }
+        }
+      )
     }
   })
 
@@ -34,7 +54,16 @@ const Animated = ({ data }) => {
       loader={PLASMIC}
       prefetchedData={plasmicComponents}
     >
-      <PlasmicComponent component="/animated" />
+      <PlasmicComponent
+        component="/animated"
+        componentProps={{
+          root: {
+            props: {
+              ref,
+            }
+          }
+        }}
+      />
     </PlasmicRootProvider>
   );
 };
