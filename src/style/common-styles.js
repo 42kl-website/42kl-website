315f613646c752ea0ac36e91924e7617d2628082
IsaaacLim <isaaclimjj@yahoo.com>
2021-09-23 17:43:30 +0800

    added background-clip to suppress warning

--- src/style/common-styles.js
+++ src/style/common-styles.js
@@ -15,6 +15,8 @@ export const Section = styled.div`
 export const Highlight = styled.span`
   font-weight: bold;
   background: -webkit-linear-gradient(45deg, #3d5aae, #00babc);
+  background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  color: white;
 `;
