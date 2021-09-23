f7219231e0b7083e4309f64a226064553b3e0c0b
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-09-24 02:51:31 +0800

    add </WORLD READY TALENT>

--- src/style/common-styles.js
+++ src/style/common-styles.js
@@ -8,6 +8,7 @@ export const Section = styled.div`
   display: flex;
   height: 100vh;
   width: 100%;
+  margin: 16px;
   align-items: center;
   justify-content: center;
 `;
@@ -15,6 +16,19 @@ export const Section = styled.div`
 export const Highlight = styled.span`
   font-weight: bold;
   background: -webkit-linear-gradient(45deg, #3d5aae, #00babc);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+export const BigHighlight = styled.p`
+  margin: 0px;
+  font-size: ${ props => props.fontSize || "10em" };
+  user-select: none;
+  font-weight: bold;
+  background: #00BABC;
+  background: -webkit-radial-gradient(circle farthest-corner at center center, #00BABC 14%, #3D5AAE 100%);
+  background: -moz-radial-gradient(circle farthest-corner at center center, #00BABC 14%, #3D5AAE 100%);
+  background: radial-gradient(circle farthest-corner at center center, #00BABC 14%, #3D5AAE 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 `;
