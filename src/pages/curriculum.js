65c4e801ed9cc4b9565b1d3dba64bbf9443b6025
IsaaacLim <isaaclimjj@yahoo.com>
2021-10-07 23:17:40 +0800

    used curriculum as layout test page

--- src/pages/curriculum.js
+++ src/pages/curriculum.js
@@ -1,8 +1,31 @@
 import * as React from "react";
 import LayoutHomePage from "../components/layout-homepage";
+import styled from "styled-components";
+// import { Section, Highlight, BigHighlight } from "../style/common-styles";
+
+export const Section = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.direction || "row"}; //"Passed Props"
+  height: 100vh;
+  /* width: 100%; */
+  /* margin: 16px; */
+  align-items: center;
+  /* justify-content: center; */
+  justify-content: space-between;
+  padding-left: 10%;
+  padding-right: 10%;
+  /* padding-bottom: 60px; */
+`;
 
 const Curriculum = () => {
-  return <LayoutHomePage>Curriculum Page</LayoutHomePage>;
+  return (
+    <LayoutHomePage>
+      <Section>
+        Curriculum Page
+        <div>HELLO</div>
+      </Section>
+    </LayoutHomePage>
+  );
 };
 
 export default Curriculum;
