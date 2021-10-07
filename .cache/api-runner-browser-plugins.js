b82a313c2294e5e8605f9b7950522d965974c402
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-10-07 18:48:41 +0800

    Update filename

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-gatsby-cloud/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
