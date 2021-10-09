b81db978df862f95a0a8c35c1b083e1dc260b94b
IsaaacLim <isaaclimjj@yahoo.com>
2021-10-09 11:54:46 +0800

    tested Static Image backgroundsizing

--- src/pages/curriculum.js
+++ src/pages/curriculum.js
@@ -2,8 +2,9 @@ import * as React from "react";
 import LayoutHomePage from "../components/layout-homepage";
 import styled from "styled-components";
 // import { Section, Highlight, BigHighlight } from "../style/common-styles";
+import { StaticImage } from "gatsby-plugin-image";
 
-export const Section = styled.div`
+const Section = styled.div`
   /* display: flex; */
   flex-direction: ${(props) => props.direction || "row"}; //"Passed Props"
   height: 100vh;
@@ -20,15 +21,32 @@ export const Section = styled.div`
   position: relative;
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  border-style: solid;
+  border-color: #fff;
+  object-fit: contain;
+  object-position: 50% 50%;
+`;
 const Curriculum = () => {
   return (
     <LayoutHomePage>
       <Section>
-        Curriculum Page
-        <div style={{ position: "absolute", right: "10%", bottom: "15%" }}>
-          HELLO
-        </div>
+        Curriculum Page: THIS PAGE IS CURRENTLY BEING USED AS A TEST SITE BY
+        ISAAC!!!
       </Section>
+      <Container>
+        <StaticImage
+          src="../images/iMacs-on-campus.png"
+          style={{
+            height: "100vh",
+          }}
+          imgStyle={{
+            objectFit: "cover",
+            objectPosition: "center bottom",
+          }}
+        />
+      </Container>
     </LayoutHomePage>
   );
 };
